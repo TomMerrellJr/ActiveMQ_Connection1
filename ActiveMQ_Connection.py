7f9564fa88e0c56bbdc2e0184aebc94b7ed6c7f9
Tom Merrell Jr <merrell.8@wright.edu>
2018-08-21 23:22:49 -0400

    Added some comments

--- ActiveMQ_Connection.py
+++ ActiveMQ_Connection.py
@@ -8,8 +8,8 @@ import paho.mqtt.publish as publish
 import json
 import time
 
-broker = "localhost"
-port = 1883
+broker = "localhost"  # The mqtt broker location (can be changed to an IP address if so desired
+port = 1883  # The mqtt broker port
 
 MSGDICT = {}
 workload = 0
@@ -19,25 +19,32 @@ workload = 0
 def on_message(client, userdata, msg):
     global MSGDICT
     global workload
-    MSGDICT = dict(json.loads(msg.payload))
-    workload = MSGDICT['outputValues']['value']
+    MSGDICT = dict(json.loads(msg.payload))  # Convert the message payload to a dictionary that can be easily used.
+    workload = MSGDICT['outputValues']['value']  # This is where the workload value can be found in the sample output
+    # This loop here can be altered to send specific messages to the MCITOPIC for the phone to display.
     if workload <= 33:
+        # Send 3 sample messages for the phone to read, each 5 seconds apart
         publish.single("MCITOPIC", "(H) This is a sample high priority alert", 0)
         time.sleep(5)
         publish.single("MCITOPIC", "(M) This is a sample moderate priority alert", 0)
         time.sleep(5)
         publish.single("MCITOPIC", "(L) This is a sample low priority alert", 0)
     elif 33 < workload <= 67:
+        # Send 2 sample messages for the phone to read each 5 seconds apart
         publish.single("MCITOPIC", "(H) this is a sample high priority alert", 0)
         time.sleep(5)
         publish.single("MCITOPIC", "(M) This is a sample moderate priority alert", 0)
     elif workload > 67:
+        # only send the "high" priority alert for the phone to read
         publish.single("MCITOPIC", "(H) This is a sample high priority alert", 0)
+    else:
+        print("Cannot find the mental workload value")
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("FuSE/ModelOutput")
+    client.subscribe("Toolkit.DataMessage.JSON")
+    publish.single("MCITOPIC", "(S)", 0)  # this will be used to sync the WAD timer with the connection of this program
 
 
 client = mqtt.Client()
@@ -45,5 +52,7 @@ client.connect(broker, port)
 
 client.on_connect = on_connect
 client.on_message = on_message
+
+time.sleep(2)
 
 client.loop_forever()
